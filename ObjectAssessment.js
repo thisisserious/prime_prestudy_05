e9388d3d165d3224a0c60626e1111960e5c17a9d
Laura Abend <laura.l.abend@gmail.com>
2016-09-07 17:27:16 -0500

    Add js & html files

---
+++ ObjectAssessment.js
@@ -0,0 +1,10 @@
+var avengersMovie = {
+	name: "Avengers",
+	runtime: "143 mins",
+	releaseYear: "2012",
+	"Director": "Joss Whedon"
+};
+
+if (avengersMovie.name == "Avengers") {
+  console.log (avengersMovie);
+}
